25621052fa364df558074e015b267f4c20517dbe
alexkong1123 <103373823+alexkong1123@users.noreply.github.com>
2022-04-30 18:32:03 +0800

    Update main.js

--- 稱號系統 (已解壓版本) 改稱號在這裏改/blockly-gametest/main.js
+++ 稱號系統 (已解壓版本) 改稱號在這裏改/blockly-gametest/main.js
@@ -10,13 +10,13 @@ var _E7_8E_A9_E5_AE_B6, _E8_A8_8A_E6_81_AF;
     _E8_A8_8A_E6_81_AF = e.message;
       if (_E7_8E_A9_E5_AE_B6.hasTag('mod')) {
     log((['[§e房主§r]',_E7_8E_A9_E5_AE_B6.name,'§l>>§r',_E8_A8_8A_E6_81_AF].join('')))
-    //稱號1
+    //稱號1 //tag:mod
   } else if (_E7_8E_A9_E5_AE_B6.hasTag('vips')) {
     log((['[§gVIP§r]',_E7_8E_A9_E5_AE_B6.name,'§l>>§r',_E8_A8_8A_E6_81_AF].join('')))
-    //稱號2
+    //稱號2 //tag:vips
   } else if (_E7_8E_A9_E5_AE_B6.hasTag('ops')) {
     log((['[§c管理員§r]',_E7_8E_A9_E5_AE_B6.name,'§l>>§r',_E8_A8_8A_E6_81_AF].join('')))
-    //稱號3
+    //稱號3 //tag:ops
   } else {
     log((['<',_E7_8E_A9_E5_AE_B6.name,'> ',_E8_A8_8A_E6_81_AF].join('')))
   }
